fd83c21c976c7f5c1d6bbd805b0461caf6f2a5c4
Michael Rios <mrios37@gatech.edu>
2021-07-17 14:43:07 -0700

    Cleanup.

--- speedtest.py
+++ speedtest.py
@@ -1,13 +1,17 @@
 import json
 import logging
 import os
+import signal
 import subprocess
+import sys
+import time
 from datetime import datetime
 
+import urllib3.exceptions
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)s %(message)s")
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s")
 log = logging.getLogger(__name__)
 
 
@@ -17,32 +21,46 @@ class SpeedtestWriter:
             token = os.environ.get("INFLUXDB_TOKEN")
 
         # initialize influx client
-        client = InfluxDBClient(url=influx_url, token=token)
+        self.influx_client = InfluxDBClient(url=influx_url, token=token)
         self.influx_org = "org"
         self.influx_bucket = "speedtest_results"
-        self.influx_api = client.write_api(write_options=SYNCHRONOUS)
+        self.influx_api = self.influx_client.write_api(write_options=SYNCHRONOUS)
+
+        signal.signal(signal.SIGINT, self.shutdown)
+
+    def shutdown(self, *args):
+        log.info("Shutting Down.")
+        sys.exit(0)
 
     def run_speedtest(self):
+        # Currently no point in running speedtest if influx is down, maybe store buffer of results
+        if not self._is_influx_ready():
+            log.error("InfluxDB status check failed.")
+            return None
+
         try:
-            log.info("Running speedtest.")
-            res = subprocess.run(
+            log.info("Running speedtest...")
+            self.p = subprocess.run(
                 ["speedtest", "--accept-license", "-f", "json"],
                 input="yes\n".encode(),
                 timeout=5 * 60,
                 capture_output=True,
                 check=True,
             )
-            res = json.loads(res.stdout)
+            return json.loads(self.p.stdout)
         except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as e:
             log.error(f"Issue getting speedtest results. {e}")
-            res = None
-        finally:
-            return res
+            return None
 
     def write_results(self, data):
         ts = datetime.fromisoformat(data["timestamp"].replace("Z", "+00:00"))
         measurments = ["ping", "download", "upload", "server"]
         log.debug(data)
+
+        if not self._is_influx_ready():
+            log.error("InfluxDB status check failed.")
+            return
+
         for mem, value in data.items():
             if mem in measurments:
                 for k, v in value.items():
@@ -52,6 +70,23 @@ class SpeedtestWriter:
                         point = Point(mem).field(f"{k}_mbps", self.bps_to_mbps(v)).time(ts, WritePrecision.NS)
                         self.influx_api.write(self.influx_bucket, self.influx_org, point)
 
+    def run(self, weeks=0, days=0, hours=0, minutes=0):
+        interval = (weeks * 604800) + (days * 86400) + (hours * 3600) + (minutes * 60)
+        log.info(f"Running w/ interval of {interval} seconds.")
+
+        while True:
+            res = self.run_speedtest()
+            if res is not None:
+                s.write_results(res)
+            time.sleep(interval)
+
+    def _is_influx_ready(self):
+        try:
+            log.info(f"InfluxDB status: {self.influx_client.ready().status}")
+            return True
+        except urllib3.exceptions.NewConnectionError as e:
+            return False
+
     @staticmethod
     def bps_to_mbps(b):
         """convert bytest per second to Megabits per second"""
@@ -60,6 +95,4 @@ class SpeedtestWriter:
 
 if __name__ == "__main__":
     s = SpeedtestWriter()
-    res = s.run_speedtest()
-    if res is not None:
-        s.write_results(res)
+    s.run(minutes=1)
